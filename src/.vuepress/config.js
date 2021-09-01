dac8bd1d975036b18c3457d66f3f7c9a9dcf209a
chenzhunrong <chenzhunrong@dm-ai.cn>
2021-09-01 10:53:17 +0800

    调整目录

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   title: "WebGL Notes",
   description: "WebGL学习、开发笔记",
-  dest: path.resolve(__dirname, '../../dist'),
+  dest: path.resolve(__dirname, '../../docs'),
   themeConfig: {
     sidebar: [
       "/",
